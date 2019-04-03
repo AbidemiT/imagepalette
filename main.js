0848fdf86b068a3860bda9a05723f32fc0077389
AbidemiT <abidemis01@gmail.com>
2019-04-03 17:59:12 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let buttons = document.querySelectorAll('button');
 let img = document.querySelectorAll('img');
 let body = document.getElementById('body');
 
-buttons.forEach((button,i) => {
+buttons.forEach((button, i) => {
   button.addEventListener('click', () => {
     body.style.backgroundImage = `url(${img[i].currentSrc})`;
     body.style.backgroundSize = 'cover';
   });
-})
+});
